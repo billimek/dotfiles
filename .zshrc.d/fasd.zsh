61272c4c45cb4f5fb697fe9ea24488c650e6ca9b
Jeff Billimek <jeffrey_k_billimek@homedepot.com.com>
2018-12-12 12:49:19 -0800

    formatting & fixing fasd

--- .zshrc.d/fasd.zsh
+++ .zshrc.d/fasd.zsh
@@ -1,12 +1,12 @@
 #
 # only init if installed.
-fasd_cache="$HOME/.fasd-init-bash"
+#fasd_cache="$HOME/.fasd-init-bash"
 #if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
 if [ "$(command -v fasd)" ]; then
   eval "$(fasd --init posix-alias zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install)" >| "$fasd_cache"
 fi
-source "$fasd_cache"
-unset fasd_cache
+#source "$fasd_cache"
+#unset fasd_cache
 
 
 # jump to recently used items
