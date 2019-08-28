5b37a8e29c5b28714f8442ef564d53f7dee7fda1
Jeff Billimek <jeff@billimek.com>
2019-08-28 08:02:48 -0400

    new gcloud location

--- .zshrc.d/google.zsh
+++ .zshrc.d/google.zsh
@@ -1,9 +1,5 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [[ -s '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc' ]]; then
-  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
-fi
+if [ -f '/Users/jeff/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/jeff/google-cloud-sdk/path.zsh.inc'; fi
 
-# The next line enables shell command completion for gcloud.
-if [[ -s '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc' ]]; then
-  source '/opt/homebrew-cask/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
-fi
+# # The next line enables shell command completion for gcloud.
+if [ -f '/Users/jeff/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/jeff/google-cloud-sdk/completion.zsh.inc'; fi
