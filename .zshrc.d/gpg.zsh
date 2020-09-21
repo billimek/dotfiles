a1120b0fbff2e368c1cd2338e6199732163fe35f
Jeff Billimek <jeff@billimek.com>
2020-09-21 08:31:24 -0400

    may not need custom ggp settings because of plugin

--- .zshrc.d/gpg.zsh
+++ .zshrc.d/gpg.zsh
@@ -1,4 +1,4 @@
-export GPG_TTY="$(tty)"
-export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
-gpgconf --launch gpg-agent
+# export GPG_TTY="$(tty)"
+# export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
+# gpgconf --launch gpg-agent
 
