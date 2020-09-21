f7f7b1d2630791ee25a3b5a46a7c87e84f98603f
Jeff Billimek <jeff@billimek.com>
2020-09-21 08:55:07 -0400

    using a different gpg plugin

--- .zshrc.d/gpg.zsh
+++ .zshrc.d/gpg.zsh
@@ -2,3 +2,10 @@
 # export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 # gpgconf --launch gpg-agent
 
+if [[ $OSTYPE == darwin* ]]; then
+  _GPG_AGENT_SOCK="${HOME}/.gnupg/S.gpg-agent"
+  _GPG_AGENT_SSH_SOCK="${HOME}/.gnupg/S.gpg-agent.ssh"
+elif [[ $OSTYPE == linux* ]]; then
+  _GPG_AGENT_SOCK="${XDG_RUNTIME_DIR}/.gnupg/S.gpg-agent"
+  _GPG_AGENT_SSH_SOCK="${XDG_RUNTIME_DIR}/.gnupg/S.gpg-agent.ssh"
+fi
