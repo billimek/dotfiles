8e5d0355401658c24f672f976ce0b7b40e6e4732
Jeff Billimek <jeff@billimek.com>
2020-10-14 17:03:35 -0400

    handle codespaces bootstrapping
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- setup.sh
+++ setup.sh
@@ -8,6 +8,31 @@ print_header() {
 }
 
 # OS Specific Setup with yadm
+
+if [ "$CODESPACES" = "true" ]; then
+  print_header "In a codespaces environment"
+  # only execute if this is the first time
+  if ! which yadm; then
+    echo "installing yadm"
+    sudo curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && sudo chmod a+x /usr/local/bin/yadm
+
+    echo "removing conflicting dotfiles"
+    mv ~/.gitconfig ~/.gitconfig.orig
+    mv ~/.zshrc ~/.zshrc.orig
+
+    echo "initialize yadm"
+    /usr/local/bin/yadm clone https://github.com/billimek/dotfiles.git --no-bootstrap
+
+    echo "install starship prompt"
+    curl -fsSL https://starship.rs/install.sh | bash -s -- -f
+
+    echo "changing shell to zsh"
+    sudo chsh -s /usr/bin/zsh codespace
+  fi
+  # exit setup script early
+  exit 0
+fi
+
 if [ "$OSTYPE" = "darwin"* ]; then
   if ! which brew; then
     echo "=============== Installing homebrew"
