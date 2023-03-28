81e1f2d9bb5ecdb0e3be32bee5db6a3b064ff9c6
Jeff Billimek <billimek@homedepot.com>
2023-03-28 14:58:46 -0400

    making google.zsh portable

--- .zshrc.d/google.zsh
+++ .zshrc.d/google.zsh
@@ -1,5 +1,5 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/jeff/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/jeff/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f '$HOME/tmp/google-cloud-sdk/path.zsh.inc' ]; then . '$HOME/tmp/google-cloud-sdk/path.zsh.inc'; fi
 
-# # The next line enables shell command completion for gcloud.
-if [ -f '/Users/jeff/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/jeff/google-cloud-sdk/completion.zsh.inc'; fi
+# The next line enables shell command completion for gcloud.
+if [ -f '$HOME/tmp/google-cloud-sdk/completion.zsh.inc' ]; then . '$HOME/tmp/google-cloud-sdk/completion.zsh.inc'; fi
