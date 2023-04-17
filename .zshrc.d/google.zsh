deb552882850b5e1da4afef06659709fb4b19bee
Jeff Billimek <jeff@billimek.com>
2023-04-17 16:38:35 -0400

    fix name and path for gcloud tools

--- .zshrc.d/google.zsh
+++ .zshrc.d/google.zsh
@@ -1,5 +1,6 @@
 # The next line updates PATH for the Google Cloud SDK.
-if [ -f '$HOME/tmp/google-cloud-sdk/path.zsh.inc' ]; then . '$HOME/tmp/google-cloud-sdk/path.zsh.inc'; fi
+if [ -f ~/google-cloud-sdk/path.zsh.inc ]; then source ~/google-cloud-sdk/path.zsh.inc; fi
 
 # The next line enables shell command completion for gcloud.
-if [ -f '$HOME/tmp/google-cloud-sdk/completion.zsh.inc' ]; then . '$HOME/tmp/google-cloud-sdk/completion.zsh.inc'; fi
+if [ -f ~/google-cloud-sdk/completion.zsh.inc ]; then source ~/google-cloud-sdk/completion.zsh.inc; fi
+
