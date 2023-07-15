9332203fb3e2b2166f1e0600fc0eb2e4c72a6d76
Jeff Billimek <jeff@billimek.com>
2023-07-15 10:43:26 -0400

    disable swap for k8s
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- hosts/k3s-g/hardware-configuration.nix
+++ hosts/k3s-g/hardware-configuration.nix
@@ -21,9 +21,11 @@
       fsType = "vfat";
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-label/swap"; }
-    ];
+  # don't use swap for kubernetes nodes
+  # swapDevices =
+  #   [ { device = "/dev/disk/by-label/swap"; }
+  #   ];
+  swapDevices = lib.mkForce [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
