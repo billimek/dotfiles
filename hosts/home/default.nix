480af7885e7a839b8cd2539905f8118cd8101342
billimek <jeff@billimek.com>
2023-07-16 10:32:41 -0400

    fix issues with network service failing rebuild
    Signed-off-by: billimek <jeff@billimek.com>

--- hosts/home/default.nix
+++ hosts/home/default.nix
@@ -14,6 +14,11 @@
     hostName = "home";
     networkmanager.enable = true;  # Easiest to use and most distros use this by default.
   };
+
+  # may fix issues with network service failing during a nixos-rebuild
+  systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
+  systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 
