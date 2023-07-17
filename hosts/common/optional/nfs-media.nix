96fcdf63b0ec2a3f271720aab4d5da4a9b3eff4b
Jeff Billimek <jeff@billimek.com>
2023-07-17 11:26:14 -0400

    clean-up
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- hosts/common/optional/nfs-media.nix
+++ hosts/common/optional/nfs-media.nix
@@ -1,11 +1,5 @@
 { config, pkgs, ... }:
 {
-  # environment.systemPackages = [
-  #   pkgs.nfs-utils
-  # ];
-
-  # boot.supportedFilesystems = [ "nfs" ];
-  # services.rpcbind.enable = true; # needed for NFS
   systemd.mounts = let commonMountOptions = {
     type = "nfs";
     mountConfig = {
--- hosts/k3s-g/default.nix
+++ hosts/k3s-g/default.nix
@@ -23,40 +23,6 @@
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
 
-#  systemd.network = {
-#    enable = true;
-#    wait-online.anyInterface = true;
-#    netdevs = {
-#      "20-vlan20" = {
-#        netdevConfig = {
-#          Kind = "vlan";
-#          Name = "vlan20";
-#        };
-#        vlanConfig.Id = 20;
-#      };
-#    };
-#    networks = {
-#      "30-enp4s0" = {
-#        matchConfig.Name = "enp4s0";
-#        # using dhcp for vlan20 instead
-#        networkConfig.DHCP = "no";
-#        #linkConfig.RequiredForOnline = "no";
-#        # tag vlan on this link
-#        vlan = [
-#          "vlan20"
-#        ];
-#      };
-#      "40-vlan20" = {
-#        matchConfig.Name = "vlan20";
-#        # acquire a DHCP lease on link up
-#        networkConfig.DHCP = "yes";
-#        # ensure that all k8s nodes use the router ntp server for clock consistency
-#        networkConfig.NTP = "10.0.7.1";
-#        networkConfig.IPv6AcceptRA = true;
-#      };
-#    };
-#  };
-
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 
