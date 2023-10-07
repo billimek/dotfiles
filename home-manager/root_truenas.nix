eb7244b9cbbcf089a4db49cdbf28642067df0608
billimek <jeff@billimek.com>
2023-10-07 08:58:48 -0400

    Patching fish to temp fix [this issue](https://github.com/fish-shell/fish-shell/issues/9705)

--- home-manager/root_truenas.nix
+++ home-manager/root_truenas.nix
@@ -32,4 +32,19 @@
       rehome = lib.mkForce "home-manager switch --flake $HOME/src/dotfiles/.#root@truenas";
     };
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      fish = prev.fish.overrideAttrs (o: {
+      patches = (o.patches or [ ]) ++ [
+        (pkgs.fetchpatch {
+          name = "fix-zfs-completion.path";
+          url = "https://github.com/fish-shell/fish-shell/commit/85504ca694ae099f023ae0febb363238d9c64e8d.patch";
+          sha256 = "sha256-lA0M7E/Z0NjuvppC7GZA5rWdL7c+5l+3SF5yUe7nEz8=";
+        })
+      ];
+    });
+    })
+  ];
+
 }
