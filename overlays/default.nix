43bb8c3d3506a59962a99b5b71bfedb92e359472
Jeff Billimek <jeff@billimek.com>
2023-10-28 09:23:51 -0400

    override acorn?
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- overlays/default.nix
+++ overlays/default.nix
@@ -11,7 +11,18 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+    acorn = prev.acorn.overrideAttrs (oldAttrs: rec {
+      # pname = "acorn";
+      version = "0.9.1";
+      src = prev.fetchFromGitHub {
+        owner = "acorn-io";
+        repo = "acorn";
+        rev = "v0.9.1";
+        hash = "sha256-FPnKmWKnEFVDXbDI+An3EKzleP43NEC9/dq4SJfWQrU=";
+      };
+    });
   };
+
 
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
