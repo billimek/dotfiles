887d542953a1b7033701d9852c9c6a0a19dfff34
billimek <jeff@billimek.com>
2023-11-21 08:36:53 -0500

    Update darwin defaults
    Signed-off-by: billimek <jeff@billimek.com>

--- hosts/common/darwin/defaults.nix
+++ hosts/common/darwin/defaults.nix
@@ -64,6 +64,13 @@
   ];
 
   #system-defaults.nix
+  # activationScripts are executed every time you boot the system or run `nixos-rebuild` / `darwin-rebuild`.
+  # system.activationScripts.postUserActivation.text = ''
+  #   # activateSettings -u will reload the settings from the database and apply them to the current session,
+  #   # so we do not need to logout and login again to make the changes take effect.
+  #   /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+  # '';
+
   system.keyboard = {
     enableKeyMapping = true;
   };
@@ -73,37 +80,80 @@
       ShowDayOfMonth = true;
       ShowAMPM = false;
     };
+
     dock = {
       autohide = true;
       tilesize = 64;
     };
+
     finder = {
       AppleShowAllExtensions = true;
       FXEnableExtensionChangeWarning = false;
       _FXShowPosixPathInTitle = true;
       # default to details view
-      FXPreferredViewStyle = "Nlsv";
-      FXDefaultSearchScope = "SCcf";
-      ShowPathbar = true;
+      FXPreferredViewStyle = "Nlsv"; # list view
+      FXDefaultSearchScope = "SCcf"; # search current folder by default
+      ShowStatusBar = true; # show status bar
+      ShowPathbar = true; # show path bar
     };
+
+    # trackpad
     trackpad = {
       Clicking = true;
       TrackpadThreeFingerDrag = true;
       # Dragging = true;
     };
+
+    
     SoftwareUpdate.AutomaticallyInstallMacOSUpdates = true;
+
     NSGlobalDomain = {
       AppleInterfaceStyle = "Dark"; # set dark mode
       "com.apple.swipescrolldirection" = false; # set natural scrolling to the _correct_ value
+
+      # show all file extensions
       AppleShowAllExtensions = true;
-      InitialKeyRepeat = 18;
-      KeyRepeat = 1;
+
+      # set key repeat to be faster
+      InitialKeyRepeat = 18; # default: 68
+      KeyRepeat = 1; # default: 6
+
+      # disable some auto-correct behaviors
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticDashSubstitutionEnabled = false;
+      # NSAutomaticPeriodSubstitutionEnabled = false;
+      # NSAutomaticQuoteSubstitutionEnabled = false;
+      # NSAutomaticSpellingCorrectionEnabled = false;
+
+      # Make a feedback sound when the system volume changed. This setting accepts the integers 0 or 1. Defaults to 1
+      "com.apple.sound.beep.feedback" = 1;
+
+      # expand save panel by default
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+
+      # expand print panel by default
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
     };
+
     CustomUserPreferences = {
       # Manage iTerm with files in ~/.config/iterm2
       "com.googlecode.iterm2" = {
         "PrefsCustomFolder" = "~/.config/iterm2";
         "LoadPrefsFromCustomFolder" = 1;
+      };
+      # Disable Creation of Metadata Files on Network Volumes
+      "com.apple.desktopservices".DSDontWriteNetworkStores = true;
+      # Disable Creation of Metadata Files on USB Volumes
+      "com.apple.desktopservices".DSDontWriteUSBStores = true;
+      # safari should enable dev mode
+      "com.apple.Safari" = {
+        "IncludeInternalDebugMenu" = true;
+        "IncludeDevelopMenu" = true;
+        "WebKitDeveloperExtrasEnabledPreferenceKey" = true;
+        "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" =
+          true;
       };
     };
   };
