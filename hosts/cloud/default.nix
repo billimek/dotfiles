f6366b2fd49b75593bea21bcde4265f650d80370
billimek <jeff@billimek.com>
2023-11-30 22:37:58 -0500

    fix builds on aarch64
    Signed-off-by: billimek <jeff@billimek.com>

--- hosts/cloud/default.nix
+++ hosts/cloud/default.nix
@@ -24,6 +24,9 @@
     networkmanager.enable = true; # Easiest to use and most distros use this by default.
   };
 
+  # need to disable this for aarch64 due to https://github.com/NixOS/nixpkgs/issues/258515
+  environment.enableAllTerminfo = lib.mkForce false;
+
   # may fix issues with network service failing during a nixos-rebuild
   systemd.services.NetworkManager-wait-online.enable = lib.mkForce false;
   systemd.services.systemd-networkd-wait-online.enable = lib.mkForce false;
