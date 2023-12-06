4f443ba41bce69248c1f4fda7e80398a81d30128
billimek <jeff@billimek.com>
2023-12-06 09:22:00 -0500

    feat(cli): add lazygit
    Lazygit is a command-line git client that provides a simple and intuitive interface for managing git repositories. It allows users to perform common git operations like staging files, committing changes, and viewing the commit history. This feature enhances the CLI by adding lazygit as a new tool.

--- home-manager/common/features/cli/default.nix
+++ home-manager/common/features/cli/default.nix
@@ -34,6 +34,7 @@
     hyperfine # benchmarking tool "WRITTEN IN RUST"
     jq # JSON pretty printer and manipulator
     jwt-cli # JWT tool
+    lazygit
     neofetch # show system info
     ouch # better unzip "WRITTEN IN RUST"
     procs # better ps "WRITTEN IN RUST"
