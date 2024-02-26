83c2741cb44d7124d3552087f91163856f26f932
billimek <jeff@billimek.com>
2024-02-25 19:54:52 -0500

    feat: update hardware network configuration
    Adjust network settings for better functionality and compatibility. Add new bridged interfaces and VLAN configurations. Implement network-specific configurations for different interfaces.

--- hosts/nas/hardware-configuration.nix
+++ hosts/nas/hardware-configuration.nix
@@ -32,18 +32,93 @@
   ];
 
   networking = {
-    useDHCP = false; # Disable global DHCP
-    interfaces.eno2.useDHCP = lib.mkDefault true;
-    interfaces.enp2s0.useDHCP = lib.mkDefault true;
-    interfaces.eno1.useDHCP = false; # Disable DHCP for the interface you want to add to the bridge
-    interfaces.br0.useDHCP = true;
-    interfaces.br0.macAddress = "3c:ec:ef:b5:bf:6e"; # Set the MAC address of the bridge from eno1
-    bridges = {
-      "br0" = {
-        interfaces = [ "eno1" ];
+    useDHCP = lib.mkDefault true; # Disable global DHCP
+    useNetworkd = true;
+    usePredictableInterfaceNames = true;
+    firewall.enable = false;
+    firewall.allowPing = true;
+  };
+
+  systemd.network = {
+    enable = true;
+    wait-online.anyInterface = true;
+    netdevs = {
+      "20-br0" = {
+        netdevConfig = {
+          Kind = "bridge";
+          Name = "br0";
+          # TODO: change this to the MAC address of the existing mellanox card that will actually be used when installed to the new case
+          MACAddress = "3c:ec:ef:b5:bf:6f";
+          Description = "br0 bridged interface for eno1";
+        };
+      };
+      "30-vlk8s20" = {
+        netdevConfig = {
+          Name = "vlk8s20";
+          Kind = "vlan";
+          Description = "VLAN 20 (k8s nodes)";
+        };
+        vlanConfig = {
+          Id = 20;
+        };
+      };
+      "40-k8s20" = {
+        netdevConfig = {
+          Name = "brk8s20";
+          Kind = "bridge";
+          # MACAddress = "<TBD>";
+        };
       };
     };
 
+    networks = {
+      "20-tailscale-ignore" = {
+        matchConfig.Name = "tailscale*";
+        linkConfig = {
+          Unmanaged = "yes";
+          RequiredForOnline = false;
+        };
+      };
+      # TODO: change this to enp2s0 when installed to the new case
+      "30-eno2" = {
+        matchConfig.Name = "eno2"; # will eventually be enp2s0
+        networkConfig = {
+          Bridge = "br0";
+          DHCP = "no";
+        };
+        linkConfig.RequiredForOnline = "enslaved";
+      };
+      "40-vlk8s20" = {
+        matchConfig = {Name = "vlk8s20";};
+        networkConfig = {
+          Bridge = "brk8s20";
+        };
+      };
+      "50-br0" = {
+        matchConfig.Name ="br0";
+        networkConfig = {
+          DHCP = "yes";
+        };
+        linkConfig = {
+          # or "routable" with IP addresses configured
+          RequiredForOnline = "carrier";
+        };
+      };
+      "50-brk8s20" = {
+        matchConfig.Name ="brk8s20";
+        networkConfig = {
+          DHCP = "yes";
+        };
+      };
+      "99-network-defaults-wired" = {
+        matchConfig.Name = "en* | eth* | usb*";
+        networkConfig = {
+          Description = "Fallback Wired DHCP";
+          DHCP = "yes";
+          IPForward = "yes";
+        };
+      };
+    };
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
