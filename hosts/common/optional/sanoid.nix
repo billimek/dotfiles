adc8d258274cd231cfa7b6455a6c3fc4dce962c1
billimek <jeff@billimek.com>
2024-03-08 08:47:42 -0500

    feat: update sanoid.nix to adjust snapshot intervals
    discovered if you don't set a value for each time-period, it will use the defaults which are _not_ 0
    
    Update the snapshot intervals in the sanoid.nix file to better align with the backup frequency requirements. Adjusted the hourly, monthly, and yearly snapshot intervals for 'timemachine' and 'vms' templates.

--- hosts/common/optional/sanoid.nix
+++ hosts/common/optional/sanoid.nix
@@ -8,13 +8,18 @@
     extraArgs = ["--verbose"];
     templates = {
       "timemachine" = {
+        "hourly" = 0;
         "daily" = 14;
         "monthly" = 3;
+        "yearly" = 0;
         "autosnap" = true;
         "autoprune" = true;
       };
       "vms" = {
+        "hourly" = 6;
         "daily" = 14;
+        "monthly" = 3;
+        "yearly" = 0;
         "autosnap" = true;
         "autoprune" = true;
       };
