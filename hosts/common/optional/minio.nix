366af99b34acf3b024683e260519135dd065accc
billimek <jeff@billimek.com>
2024-08-04 15:22:29 -0400

    fix(hosts): update minio data directory location
    Update the data directory location for Minio to "/mnt/ssdtank/s3". The previous location was incorrect.

--- hosts/common/optional/minio.nix
+++ hosts/common/optional/minio.nix
@@ -11,8 +11,7 @@ in
   environment.systemPackages = with pkgs; [ minio-client ];
   services.minio = {
     enable = true;
-    # TODO: rename dataDir to the actualk location when ready
-    dataDir = [ "/mnt/ssdtank/s3-new" ];
+    dataDir = [ "/mnt/ssdtank/s3" ];
     package = pkgs.minio;
     # TODO: switch these to rootCredentialsFile
     accessKey = secrets.minio.accessKey;
