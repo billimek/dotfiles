2188f62efe88ceb9a1aada760f233b8b8f8b1e24
billimek <jeff@billimek.com>
2025-03-23 20:33:02 -0400

    Add helmfile and wrapHelm plugin to Kubernetes packages
    Signed-off-by: billimek <jeff@billimek.com>

--- home-manager/common/features/kubernetes/default.nix
+++ home-manager/common/features/kubernetes/default.nix
@@ -4,6 +4,7 @@
   home.packages = with pkgs; [
     fluxcd # flux CLI
     go-task # task runner
+    helmfile # helmfile CLI
     kail # kubernetes tail
     ktop # kubernetes top
     kubectl # kubernetes CLI
@@ -11,10 +12,16 @@
     kubectl-example # output example kubernetes types
     kubectl-view-allocations # view kubernetes allocations
     kubectl-view-secret # view kubernetes secrets without piping and decoding
+    minijinja # templating engine for kubernetes
     pkgs.unstable.kubecolor # colorize kubectl output
     kubernetes-helm # helm CLI
     kustomize # kustomize CLI for sadists
     talhelper # talos helper
     talosctl # talos CLI
+    (wrapHelm kubernetes-helm {
+      plugins = with pkgs.kubernetes-helmPlugins; [
+        helm-diff
+      ];
+    })
   ];
 }
