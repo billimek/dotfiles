c99960f762739af8d3d9a11f37129f4188ef76c3
billimek <jeff@billimek.com>
2025-05-03 11:07:03 -0400

    Refactor flake variable assignments for clarity and consistency
    Signed-off-by: billimek <jeff@billimek.com>

--- hosts/common/darwin/defaults.nix
+++ hosts/common/darwin/defaults.nix
@@ -51,7 +51,7 @@
     variables = {
       EDITOR = "${lib.getBin pkgs.neovim}/bin/nvim";
       SSH_AUTH_SOCK = "$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
-      FLAKE = "/etc/nixos";
+      # FLAKE = "/etc/nixos";
     };
   };
 
