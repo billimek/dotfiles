d3929048de8ff1ed4497535f2d379130ec8663a3
Jeff Billimek <jeff@billimek.com>
2025-11-07 07:51:07 -0500

    Configure Docker service to depend on basic network.target instead of network-online.target
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -2,4 +2,8 @@
   virtualisation.docker = {
     enable = true;
   };
+
+  # Don't wait for network-online.target - just need basic network.target
+  systemd.services.docker.after = [ "network.target" ];
+  systemd.services.docker.wants = [ "network.target" ];
 }
