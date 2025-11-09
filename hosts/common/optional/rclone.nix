4a03b1200bb6ad9ba08d030c1739ab9222d90c1d
billimek <jeff@billimek.com>
2025-11-09 13:02:14 -0500

    migrate rclone from minio to garage
    Signed-off-by: billimek <jeff@billimek.com>

--- hosts/common/optional/rclone.nix
+++ hosts/common/optional/rclone.nix
@@ -7,16 +7,17 @@
 {
   imports = [ ];
 
+  # TODO: handle config/secrets via opnix
+
   # Automated rclone sync to b2
+  # Uses existing /home/nix/.config/rclone/rclone.conf with garage S3 remote configured
 
-  # TODO: handle config/secrets via nix
-
-  # volsync
+  # volsync - sync from garage S3 API instead of deprecated minio filesystem paths
   systemd.services."rclone-backup-volsync" = {
     description = "Rclone backup volsync";
     script = ''
-      # rclone should use /home/nix/.config/rclone/rclone.conf by default
-      ${pkgs.rclone}/bin/rclone --config /home/nix/.config/rclone/rclone.conf --transfers 50 --fast-list sync /mnt/ssdtank/s3/volsync b2:billimek-volsync
+      # Sync from garage S3 API (bucket-to-bucket) instead of filesystem paths
+      ${pkgs.rclone}/bin/rclone --config /home/nix/.config/rclone/rclone.conf --transfers 50 --fast-list sync garage:volsync b2:billimek-volsync
     '';
     serviceConfig = {
       Type = "oneshot";
@@ -32,12 +33,12 @@
     wantedBy = [ "timers.target" ];
   };
 
-  # postgres
+  # postgres - sync from garage S3 API instead of deprecated minio filesystem paths
   systemd.services."rclone-backup-postgres" = {
     description = "Rclone backup postgres";
     script = ''
-      # rclone should use /home/nix/.config/rclone/rclone.conf by default
-      ${pkgs.rclone}/bin/rclone --config /home/nix/.config/rclone/rclone.conf --transfers 50 --fast-list sync /mnt/ssdtank/s3/postgresql b2:billimek-postgres
+      # Sync from garage S3 API (bucket-to-bucket) instead of filesystem paths
+      ${pkgs.rclone}/bin/rclone --config /home/nix/.config/rclone/rclone.conf --transfers 50 --fast-list sync garage:postgresql b2:billimek-postgres
     '';
     serviceConfig = {
       Type = "oneshot";
@@ -52,4 +53,25 @@
     };
     wantedBy = [ "timers.target" ];
   };
+
+  # talos-backup - sync from garage S3 API instead of deprecated minio filesystem paths
+  # systemd.services."rclone-backup-talos" = {
+  #   description = "Rclone backup talos";
+  #   script = ''
+  #     # Sync from garage S3 API (bucket-to-bucket) instead of filesystem paths
+  #     ${pkgs.rclone}/bin/rclone --config /home/nix/.config/rclone/rclone.conf --transfers 50 --fast-list sync garage:talos-backup b2:billimek-talos-backup
+  #   '';
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     User = "root";
+  #   };
+  # };
+  # systemd.timers."rclone-backup-talos" = {
+  #   description = "Rclone backup timer talos";
+  #   timerConfig = {
+  #     OnCalendar = "8:00:00";
+  #     Persistent = true;
+  #   };
+  #   wantedBy = [ "timers.target" ];
+  # };
 }
