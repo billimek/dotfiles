defdcfe5cff650a49a2e88e8f90b201d4d62600a
Jeff Billimek <jeff@billimek.com>
2025-11-28 13:45:17 -0500

    Comment out unstable.tailscale in default.nix
    Signed-off-by: Jeff Billimek <jeff@billimek.com>

--- home-manager/common/features/cli/default.nix
+++ home-manager/common/features/cli/default.nix
@@ -51,7 +51,7 @@
     ripgrep # Better grep "WRITTEN IN RUST"
     sd # better sed "WRITTEN IN RUST"
     shellcheck # shell linter
-    unstable.tailscale
+    # unstable.tailscale
     tokei # better cloc "WRITTEN IN RUST"
     unixtools.watch # watch files and run commands
     unzip # unzip files
