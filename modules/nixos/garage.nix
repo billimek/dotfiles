7e70d8d44d4440e482eddb52312477c0f16210df
billimek <jeff@billimek.com>
2026-01-01 15:29:59 -0500

    re-add setup instructions
    Signed-off-by: billimek <jeff@billimek.com>

--- modules/nixos/garage.nix
+++ modules/nixos/garage.nix
@@ -11,6 +11,46 @@ in {
     enable = lib.mkEnableOption "garage object storage";
   };
 
+  # To enable this service:
+  # 1. Generate RPC secret: openssl rand -hex 32
+  # 2. Add to secrets.nix under garage.rpc_secret
+  # 3. Enable this module in hosts/nas/default.nix
+  # 4. Run: sudo nixos-rebuild switch --flake .#nas
+  # 5. Initialize cluster:
+  #    garage status
+  #    garage layout assign <node-id> -c 500G -z default
+  #    garage layout apply --version 1
+  # 6. Create buckets and keys:
+  #    garage bucket create my-bucket
+  #    garage key create my-key
+  #    garage bucket allow --read --write my-bucket --key my-key
+
+  # Data Migration from MinIO
+
+  # # Configure both endpoints
+  # mc alias set minio http://10.0.7.7:9000 <minio-access-key> <minio-secret-key>
+  # mc alias set garage http://10.0.7.7:3900 <garage-access-key> <garage-secret-key>
+
+  # # Migrate data
+  # mc mirror minio/bucket-name garage/bucket-name
+
+  # Set an expiration policy (using the aws CLI):
+
+  # aws s3api put-bucket-lifecycle-configuration \
+  #         --endpoint-url http://127.0.0.1:3900 \
+  #         --bucket talos-backup \
+  #         --lifecycle-configuration '{
+  #       "Rules": [
+  #         {
+  #           "ID": "30-day-expiration",
+  #           "Status": "Enabled",
+  #           "Expiration": {
+  #             "Days": 30
+  #           }
+  #         }
+  #       ]
+  #     }'
+
   config = lib.mkIf cfg.enable {
     services.garage = {
       enable = true;
