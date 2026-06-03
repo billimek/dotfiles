55b9c8a643dd071f7bedf0aa937453ee7ed58bff
billimek <jeff@billimek.com>
2026-06-03 10:13:38 -0400

    darwin: add --force flag to brew bundle cleanup for Homebrew compatibility

--- modules/darwin-modules/homebrew.nix
+++ modules/darwin-modules/homebrew.nix
@@ -1,60 +1,61 @@
-{ ... }: {
-  flake.darwinModules.homebrew = 
-# Homebrew package management
+{ ... }:
 {
-  config,
-  lib,
-  ...
-}: let
-  cfg = config.modules.homebrew;
-in {
-  options.modules.homebrew = {
-    enable =
-      lib.mkEnableOption "homebrew package management"
-      // {
-        default = true;
+  flake.darwinModules.homebrew =
+    # Homebrew package management
+    {
+      config,
+      lib,
+      ...
+    }:
+    let
+      cfg = config.modules.homebrew;
+    in
+    {
+      options.modules.homebrew = {
+        enable = lib.mkEnableOption "homebrew package management" // {
+          default = true;
+        };
       };
-  };
 
-  config = lib.mkIf cfg.enable {
-    homebrew = {
-      enable = true;
-      onActivation.autoUpdate = true;
-      onActivation.upgrade = true;
-      onActivation.cleanup = "uninstall";
-      global.brewfile = true;
-      brews = [
-        "cask"
-        "mas"
-      ];
-      casks = [
-        "1password"
-        "claude"
-        "cmux"
-        "discord"
-        "element"
-        "ghostty"
-        "iterm2"
-        # "itermbrowserplugin"
-        # "karabiner-elements"
-        "logi-options+"
-        "notunes"
-        "orion"
-        "spotify"
-        "shottr"
-        "visual-studio-code"
-        "visual-studio-code@insiders"
-        "vlc"
-      ];
-      masApps = {
-        "1Password for Safari" = 1569813296;
-        "Tailscale" = 1475387142;
-        "consent-o-matic" = 1606897889;
-        "Kagi Search for Safari" = 1622835804;
-        "Wipr2 for Safari" = 1662217862;
+      config = lib.mkIf cfg.enable {
+        homebrew = {
+          enable = true;
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+          onActivation.cleanup = "uninstall";
+          onActivation.extraFlags = [ "--force" ];
+          global.brewfile = true;
+          brews = [
+            "cask"
+            "mas"
+          ];
+          casks = [
+            "1password"
+            "claude"
+            "cmux"
+            "discord"
+            "element"
+            "ghostty"
+            "iterm2"
+            # "itermbrowserplugin"
+            # "karabiner-elements"
+            "logi-options+"
+            "notunes"
+            "orion"
+            "spotify"
+            "shottr"
+            "visual-studio-code"
+            "visual-studio-code@insiders"
+            "vlc"
+          ];
+          masApps = {
+            "1Password for Safari" = 1569813296;
+            "Tailscale" = 1475387142;
+            "consent-o-matic" = 1606897889;
+            "Kagi Search for Safari" = 1622835804;
+            "Wipr2 for Safari" = 1662217862;
+          };
+        };
       };
     };
-  };
-}
-  ;
 }
